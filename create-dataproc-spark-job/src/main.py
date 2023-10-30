6dbcdadb0ad74f1909cfce87344012c53d502bea
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-30 10:08:33 -0400

    added Dataproc trigger

--- create-dataproc-spark-job/src/main.py
+++ create-dataproc-spark-job/src/main.py
@@ -1,12 +1,14 @@
 import functions_framework
 import google.oauth2.id_token
 import json
+import os
 import requests
 import yaml
 
 import google.auth.transport.requests as google_requests
 
-# from googleapiclient.discovery import build
+from googleapiclient.discovery import build
+from time import sleep
 
 
 @functions_framework.cloud_event
@@ -27,6 +29,7 @@ def create_dataproc_spark_job(cloudevent):
     )
     gcs_bucket_name = full_gcs_path.split("/", maxsplit=3)[2]
     excel_source = full_gcs_path.split("/", maxsplit=3)[-1]
+    file_name = excel_source.split("/")[-1].split(".")[0]
 
     # specifying variables for triggering excel to csv cloud function
     source_excel_bucket = gcs_bucket_name
@@ -53,5 +56,53 @@ def create_dataproc_spark_job(cloudevent):
     response = requests.post(
         audience, headers=function_headers, data=json.dumps(function_data)
     )
+
+    print("Waiting for 30 seconds for the excel to csv cloud function to complete")
+
+    sleep(30)
+
+    # creating dataproc spark job
+    service = build("dataproc", "v1", cache_discovery=False)
+    dataproc_job_request = (
+        service.projects()
+        .locations()
+        .batches()
+        .create(
+            parent=f"projects/{config_data['PROJECT_ID']}/locations/{config_data['REGION']}",
+            batchId=file_name,
+            body={
+                "runtimeConfig": {"version": config_data["RUNTIME_VERSION"]},
+                "environmentConfig": {
+                    "executionConfig": {
+                        "serviceAccount": os.environ[
+                            "SERVICE_ACCOUNT_EMAIL"
+                        ],  # Github secret -> Terraform -> Cloud Function
+                    }
+                },
+                "sparkBatch": {
+                    "mainClass": config_data["SPARK_MAIN_CLASS"],
+                    "args": [
+                        "--template",
+                        config_data["SPARK_TEMPLATE_NAME"],
+                        "--templateProperty",
+                        f"project.id={config_data['PROJECT_ID']}",
+                        "--templateProperty",
+                        f"gcs.bigquery.input.location={full_gcs_path}",
+                        "--templateProperty",
+                        "gcs.bigquery.input.format=csv",
+                        "--templateProperty",
+                        f"gcs.bigquery.output.dataset={config_data['BQ_DATASET']}",
+                        "--templateProperty",
+                        f"gcs.bigquery.output.table={config_data['BQ_TABLE']}",
+                        "--templateProperty",
+                        f"gcs.bigquery.temp.bucket.name={gcs_bucket_name}",
+                    ],
+                    "jarFileUris": config_data["SPARK_JAR_FILES"],
+                },
+            },
+        )
+    )
+
+    response = dataproc_job_request.execute()
 
     return print(str(response))
